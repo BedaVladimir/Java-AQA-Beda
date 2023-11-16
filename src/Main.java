398c7c97bdc10368d2867ecf69577f546ce1a8f1
Beda Vladimir <duracellp@yandex.ru>
2023-11-16 22:34:30 +0300

    добавил эксепшены

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
         calc(str);
     }
 
-    public static String calc(String str) {
-        String[] arr = " ".split(str);
-        // проверяем, что по длине строки это математическая операция
+    public static String calc(String str) throws Exception {
+        String[] arr = str.split(" ");
+        // проверяем, что по длине строки это математическая операция.
         if (arr.length == 3) {
             int n1 = Integer.parseInt(arr[0]);
             String op = arr[1];
@@ -24,10 +24,10 @@ public class Main {
                 }
 
             } else {
-                System.out.println("Одно из чисел не попадает в интервал от 1 до 10");
+                throw new Exception("Одно из чисел не попадает в интервал от 1 до 10");
             }
         } else {
-            System.out.println("Введите строку в формате - два операнда и оператор(+,-,*,/). Например: 1 + 2");
+            throw new Exception("Введите строку в формате - два операнда и оператор(+,-,*,/). Например: 1 + 2");
         }
         return str;
     }
